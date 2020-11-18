1fb6dc63114672f28c64962e08fc5050d519a8ac
darrenapcs <lgytuan@gmail.com>
2020-11-18 09:22:35 +0000

    test

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(*args, **kwargs):
+	return HttpResponse("<h1>Hello User</h1>")
+
+def contact_view(*args, **kwargs):
+	return HttpResponse("<h1>Contact Page</h1>")
+
+def connect_view(*args, **kwargs):
+	return HttpResponse("<h1>Connect Page</h1>")
