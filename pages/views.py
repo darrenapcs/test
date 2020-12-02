e00875a3fb9db3d602e6fccc13f749a626aa657b
darrenapcs <lgytuan@gmail.com>
2020-12-02 23:50:49 +0000

    new

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
 	return render(request, "home.html", {})
@@ -11,5 +12,17 @@ def contact_view(request, *args, **kwargs):
 	#return HttpResponse("<h1>Contact Page</h1>")
 
 def connect_view(request, *args, **kwargs):
-	return render(request, "connect.html", {})
+	if request.method == 'POST' and 'run_script' in request.POST:
+
+    		# import function to run
+		print("Hello world")
+		from .wgprivnet.WGinstance import WireGuard
+
+		wg = WireGuard()
+		wg.list_peers()
+
+    		# return user to required page
+    		# return HttpResponseRedirect(reverse(app_name:view_name)
+	else:
+		return render(request, "connect.html", {})
 
